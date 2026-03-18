c6f03fc8112a9feac54f4aea412d499cc37b1d35
KigHeart <kipropcollo@students.uonbi.ac.ke>
2026-03-18 11:49:24 +0300

    Initial commit - Pico LED blink project

---
+++ blink.c
@@ -0,0 +1,19 @@
+#include "pico/stdlib.h"
+
+#define LED_PIN   25
+#define DELAY_MS  500
+
+int main() {
+    stdio_init_all();
+
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    while (true) {
+        gpio_put(LED_PIN, 1);
+        sleep_ms(DELAY_MS);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(DELAY_MS);
+    }
+    return 0;
+}
